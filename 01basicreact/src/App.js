151dde7b4592d09e2011816aafec6e5a270b0412
Arpit <arpitpatidar810985@gmail.com>
2023-12-28 00:24:38 +0530

    first-react-app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Basic React App</h1>
+      <p2> First React app </p2>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Youtube.js
@@ -0,0 +1,12 @@
+const Youtube = () => {
+
+   return(
+   <div>
+     <h2>This is the youtube Component</h2>
+   </div>
+
+   )
+
+}
+
+export default Youtube;
